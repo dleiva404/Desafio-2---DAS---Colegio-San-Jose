00150aecc47b94897d09ca35c4eb83f4925ebc45
dleiva404 <davidleiva79@gmail.com>
2026-04-25 19:05:47 -0600

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Desafio_2___DAS___Colegio_San_Jose.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
