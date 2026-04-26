e5b7b67527e87415897775f7a553d4494816f562
dleiva404 <davidleiva79@gmail.com>
2026-04-25 20:45:39 -0600

    Configuracion Program.cs con DBContext y actualizacion de gitignore por buenas practicas

--- Program.cs
+++ Program.cs
@@ -1,15 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using ColegioSanJose.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Registrar el DbContext con SQL Server
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Agregar soporte para Controladores y Vistas
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
