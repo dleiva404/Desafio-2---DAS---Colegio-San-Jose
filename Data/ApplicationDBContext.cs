9ae0b50099860f6878abb528aa671b6871b0fe44
dleiva404 <davidleiva79@gmail.com>
2026-04-25 20:03:32 -0600

    ApplicationDbContext : DbContext para Alumno, Materia y Expediente

---
+++ Data/ApplicationDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ColegioSanJose.Models;
+
+namespace ColegioSanJose.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Alumno> Alumnos { get; set; }
+        public DbSet<Materia> Materias { get; set; }
+        public DbSet<Expediente> Expedientes { get; set; }
+    }
+}
